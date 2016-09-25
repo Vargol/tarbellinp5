0a1240a93659abc988b6632ebe4b53ea9285c929
David Burnett <vargol@ntlworld.com>
2016-09-25 15:45:46 +0100

    Move to prototype for class contrsuction, reduces memory usgae.

--- Node.Garden.Alpha.js
+++ Node.Garden.Alpha.js
@@ -195,68 +195,69 @@ var Spine = function (Id) {
      this.amp[n] = random(0.01,0.3);
      this.t[n] = random(0.01,0.2);
    }
+}
 
+Spine.prototype.setPosition = function (X, Y) {
+  this.x = X;
+  this.y = Y;
+}
 
-  this.setPosition = function (X, Y) {
-    this.x = X;
-    this.y = Y;
+Spine.prototype.setTheta = function (T) {
+  this.theta = T;
+}
+
+Spine.prototype.traceInto = function (MT) {
+  // skip into the future
+  for (time=random(MT);time<MT*2;time+=random(0.1,2.0)) {
+    this.grow();
+  }
+}
+
+Spine.prototype.grow = function () {
+  // save last position
+  this.xx = this.x;
+  this.yy = this.y;
+  
+  // calculate new position
+  this.x+=this.step*cos(this.theta);
+  this.y+=this.step*sin(this.theta);
+  
+  // rotational meander
+  var thetav = 0.0;
+  for (var n=0;n<this.depth;n++) {
+    thetav+=this.amp[n]*sin(time*this.t[n]);
+    this.amp[n]*=0.9998;
+    this.t[n]*=0.998;
   }
   
-  this.setTheta = function (T) {
-    this.theta = T;
-  }
-  
-  this.traceInto = function (MT) {
-    // skip into the future
-    for (time=random(MT);time<MT*2;time+=random(0.1,2.0)) {
-      this.grow();
-    }
-  }
-
-  this.grow = function () {
-    // save last position
-    this.xx = this.x;
-    this.yy = this.y;
-    
-    // calculate new position
-    this.x+=this.step*cos(this.theta);
-    this.y+=this.step*sin(this.theta);
-    
-    // rotational meander
-    var thetav = 0.0;
-    for (var n=0;n<this.depth;n++) {
-      thetav+=this.amp[n]*sin(time*this.t[n]);
-      this.amp[n]*=0.9998;
-      this.t[n]*=0.998;
-    }
-    
-    this.step*=1.005;
+  this.step*=1.005;
 //    step*=0.995;
 //    step+=0.01;
-    this.theta+=thetav;
-    
-    // render    
-    this.draw();
-    
-    // place node?
-    if (random(1000)<61) {
-      var m = random(3.21,5+500/(1+time));
-      makeNode(this.x,this.y,m);
-    }
-  }  
+  this.theta+=thetav;
   
-  this.draw = function () {
-    stroke(85,26);
-    line(this.x,this.y,this.xx,this.yy);
+  // render    
+  this.draw();
+  
+  // place node?
+  if (random(1000)<61) {
+    var m = random(3.21,5+500/(1+time));
+    makeNode(this.x,this.y,m);
   }
-  
+}  
+
+Spine.prototype.draw = function () {
+  stroke(85,26);
+  line(this.x,this.y,this.xx,this.yy);
 }
+  
 
 
 
 // -----------------------
 
 function GNode(Id) {
+  
+  
   var id;
   var x, y;
   var mass;
@@ -270,157 +271,153 @@ function GNode(Id) {
   
   var myc;
       
-    // set identification number
-    this.id = Id;
-    // create connection list
-    this.cons = [];
-    // initialize one time
+  // set identification number
+  this.id = Id;
+  // create connection list
+  this.cons = [];
+  // initialize one time
 
-  this.initSelf = function () {
-    this.maxcons = 11;
-    this.cons = [];
-    // initialize connections
-    this.initConnections();
-    // pick color
-    this.myc = somecolor();
-    this.hidden = false;
-  }
+  this.maxcons = 11;
+  this.cons = [];
+  // initialize connections
+  this.initConnections();
+  // pick color
+  this.myc = somecolor();
+  this.hidden = false;
+
+}
   
-  this.initConnections = function () {
-    // set number of connections to zero
-    this.numcons=0;
-  }
-  
-  this.calcHidden = function () {
-    // determine if hidden by larger gnode
-    for (var n=0;n<numNodes;n++) {
-      if (n!=id) {
-        if (gnodes[n].mass>this.mass) {
-          var d = dist(this.x,this.y,gnodes[n].x,gnodes[n].y);
-          if (d<abs(this.mass*0.321-gnodes[n].mass*0.321)) {
-              this.hidden = true;
-          }
+GNode.prototype.initConnections = function () {
+  // set number of connections to zero
+  this.numcons=0;
+}
+
+GNode.prototype.calcHidden = function () {
+  // determine if hidden by larger gnode
+  for (var n=0;n<numNodes;n++) {
+    if (n!=this.id) {
+      if (gnodes[n].mass>this.mass) {
+        var d = dist(this.x,this.y,gnodes[n].x,gnodes[n].y);
+        if (d<abs(this.mass*0.321-gnodes[n].mass*0.321)) {
+            this.hidden = true;
         }
       }
     }
   }
-  
-  this.setPosition = function (X, Y) {
-    // position self
-    this.x=X;
-    this.y=Y;
-  }
-  
-  this.setMass = function (Sz) {
-    // set size
-    this.mass=Sz;
-  }
-   
-  this.findRandomConnection = function () {
-    // check for available connection element
-    if ((this.numcons<this.maxcons) && (this.numcons<this.mass)) {
-      // pick other gnode at large
-      var cid = int(random(this.numNodes));
-      if (cid!=this.id) {
-        this.cons[this.numcons]=this.cid;
-        this.numcons++;
+}
+
+GNode.prototype.setPosition = function (X, Y) {
+  // position self
+  this.x=X;
+  this.y=Y;
+}
+
+GNode.prototype.setMass = function (Sz) {
+  // set size
+  this.mass=Sz;
+}
+ 
+GNode.prototype.findRandomConnection = function () {
+  // check for available connection element
+  if ((this.numcons<this.maxcons) && (this.numcons<this.mass)) {
+    // pick other gnode at large
+    var cid = int(random(this.numNodes));
+    if (cid!=this.id) {
+      this.cons[this.numcons]=this.cid;
+      this.numcons++;
 //        println(id+" connected to "+cid);
-      } else {
-        // random connection failed
-      }
     } else {
-      // no connection elements available
+      // random connection failed
     }
+  } else {
+    // no connection elements available
   }
+}
+
+GNode.prototype.findNearConnection = function () {
+  // find closest node
+  if ((this.numcons<this.maxcons) && (this.numcons<this.mass)) {
+    // sample 5% of nodes for near connection
+    var dd = dim;
+    var dcid = -1;
+    for (var k=0;k<(numNodes/20);k++) {
+      var cid = int(random(numNodes-1));
+      var d = sqrt((this.x-gnodes[cid].x)*(this.x-gnodes[cid].x)+(this.y-gnodes[cid].y)*(this.y-gnodes[cid].y));
+      if ((d<dd) && (d<this.mass*6)) {
+        // closer gnode has been found
+        dcid = cid;
+        dd = d;
+      }
+    }
   
-  this.findNearConnection = function () {
-    // find closest node
-    if ((this.numcons<this.maxcons) && (this.numcons<this.mass)) {
-      // sample 5% of nodes for near connection
-      var dd = dim;
-      var dcid = -1;
-      for (var k=0;k<(numNodes/20);k++) {
-        var cid = int(random(numNodes-1));
-        var d = sqrt((this.x-gnodes[cid].x)*(this.x-gnodes[cid].x)+(this.y-gnodes[cid].y)*(this.y-gnodes[cid].y));
-        if ((d<dd) && (d<this.mass*6)) {
-          // closer gnode has been found
-          dcid = cid;
-          dd = d;
-        }
-      }
-    
-      if (dcid>=0) {
-        // close node has been found, connect to it
-        this.connectTo(dcid);
-      }
+    if (dcid>=0) {
+      // close node has been found, connect to it
+      this.connectTo(dcid);
     }
   }
+}
 
-  this.connectTo = function (Id) {
-    if (this.numcons<this.maxcons) {
-      var duplicate = false;
-      for (var n=0;n<this.numcons;n++) {
-        if (this.cons[n]==Id) {
-          duplicate = true;
-        }
-      }
-      if (!duplicate) {
-        this.cons[this.numcons]=Id;
-        this.numcons++;  
-      }
-    }
-  }
-                         
-  this.drawNodeDark = function () {
-    // stamp node icon down
-    if (!this.hidden) {
-      var half_mass = this.mass/2;
-    //  blend(nodeIcoDark,0,0,nodeIcoDark.width,nodeIcoDark.height,int(x-half_mass),int(y-half_mass),int(mass),int(mass),DARKEST);  
-        image(nodeIcoDark,int(this.x-half_mass),int(this.y-half_mass),int(this.mass),int(this.mass));
-        
-    }
-  }
-
-  this.drawNodeSpecular = function () {
-    // stamp node specular
-    if (!this.hidden) {
-      var half_mass = this.mass/2;
-    //  blend(nodeIcoSpec,0,0,nodeIcoSpec.width,nodeIcoSpec.height,int(x-half_mass),int(y-half_mass),int(mass),int(mass),LIGHTEST);  
-      image(nodeIcoSpec,int(this.x-half_mass),int(this.y-half_mass),int(this.mass),int(this.mass));
-    }
-  }
-
-  this.drawNodeBase = function () {
-    // stamp node base
-    if (!this.hidden) {
-      var half_mass = this.mass/2;
-      image(nodeIcoBase,int(this.x-half_mass),int(this.y-half_mass),int(this.mass),int(this.mass));
-//      blend(nodeIcoBase,0,0,nodeIcoBase.width,nodeIcoBase.height,int(x-half_mass),int(y-half_mass),int(mass),int(mass),DARKEST);  
-    }
-  }
-      
-                  
-  this.drawConnections = function () {
+GNode.prototype.connectTo = function (Id) {
+  if (this.numcons<this.maxcons) {
+    var duplicate = false;
     for (var n=0;n<this.numcons;n++) {
-      // calculate connection distance
-      var d = 4*dist(this.x,this.y,gnodes[this.cons[n]].x,gnodes[this.cons[n]].y);
-      for (var i=0;i<d;i++) {
-        // draw several points between connected gnodes  
-        var a = i/d;
-        // fuzz
-        var fx = random(-0.42,0.42);
-        var fy = random(-0.42,0.42);
-        var cx = fx + this.x+(gnodes[this.cons[n]].x-this.x)*a;
-        var cy = fy + this.y+(gnodes[this.cons[n]].y-this.y)*a;
-        
-        stroke(red(this.myc),green(this.myc),blue(this.myc),256*(0.05+(1-sin(a*PI))*0.16));
-        point(cx,cy);
+      if (this.cons[n]==Id) {
+        duplicate = true;
       }
-    }   
+    }
+    if (!duplicate) {
+      this.cons[this.numcons]=Id;
+      this.numcons++;  
+    }
   }
-  
-  this.initSelf();
+}
+                       
+GNode.prototype.drawNodeDark = function () {
+  // stamp node icon down
+  if (!this.hidden) {
+    var half_mass = this.mass/2;
+  //  blend(nodeIcoDark,0,0,nodeIcoDark.width,nodeIcoDark.height,int(x-half_mass),int(y-half_mass),int(mass),int(mass),DARKEST);  
+      image(nodeIcoDark,int(this.x-half_mass),int(this.y-half_mass),int(this.mass),int(this.mass));
+      
+  }
+}
 
+GNode.prototype.drawNodeSpecular = function () {
+  // stamp node specular
+  if (!this.hidden) {
+    var half_mass = this.mass/2;
+  //  blend(nodeIcoSpec,0,0,nodeIcoSpec.width,nodeIcoSpec.height,int(x-half_mass),int(y-half_mass),int(mass),int(mass),LIGHTEST);  
+    image(nodeIcoSpec,int(this.x-half_mass),int(this.y-half_mass),int(this.mass),int(this.mass));
+  }
+}
+
+GNode.prototype.drawNodeBase = function () {
+  // stamp node base
+  if (!this.hidden) {
+    var half_mass = this.mass/2;
+    image(nodeIcoBase,int(this.x-half_mass),int(this.y-half_mass),int(this.mass),int(this.mass));
+//      blend(nodeIcoBase,0,0,nodeIcoBase.width,nodeIcoBase.height,int(x-half_mass),int(y-half_mass),int(mass),int(mass),DARKEST);  
+  }
+}
+    
+                
+GNode.prototype.drawConnections = function () {
+  for (var n=0;n<this.numcons;n++) {
+    // calculate connection distance
+    var d = 4*dist(this.x,this.y,gnodes[this.cons[n]].x,gnodes[this.cons[n]].y);
+    for (var i=0;i<d;i++) {
+      // draw several points between connected gnodes  
+      var a = i/d;
+      // fuzz
+      var fx = random(-0.42,0.42);
+      var fy = random(-0.42,0.42);
+      var cx = fx + this.x+(gnodes[this.cons[n]].x-this.x)*a;
+      var cy = fy + this.y+(gnodes[this.cons[n]].y-this.y)*a;
+      
+      stroke(red(this.myc),green(this.myc),blue(this.myc),256*(0.05+(1-sin(a*PI))*0.16));
+      point(cx,cy);
+    }
+  }   
 }
 
 
